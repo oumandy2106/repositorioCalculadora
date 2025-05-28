383da79413303fcdefa4ecea708e44f12a43c6fa
oumandy2106 <20243ds039@utez.edu.mx>
2025-05-28 08:46:58 -0600

    Le cambie el main de privado a publico

--- U1_T1_04_Computadora/src/Main.java
+++ U1_T1_04_Computadora/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    private static void main(String[] args) {
+    public static void main(String[] args) {
         computadora pc1 = new computadora("Thinkpad", "3");
         pc1.encender();
         pc1.apagar();
