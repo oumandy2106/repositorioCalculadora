ffc15b042b2318341dc312d575d6b9ec822ade5b
oumandy2106 <20243ds039@utez.edu.mx>
2025-05-28 08:36:34 -0600

    Termine el codigo aunque no me deja ejecutar el archivo

--- U1_T1_04_Computadora/src/Main.java
+++ U1_T1_04_Computadora/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     private static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        computadora pc1 = new computadora("Thinkpad", "3");
+        pc1.encender();
+        pc1.apagar();
+        pc1.encender();
     }
 }
--- U1_T1_04_Computadora/src/computadora.java
+++ U1_T1_04_Computadora/src/computadora.java
@@ -1,6 +1,38 @@
-public class computadora {
+    public class computadora {
     private String marca;
     private String modelo;
     private boolean encendido;
-}
 
+    public computadora(String marca, String modelo ){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.encendido = false;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+        }
+        public boolean isEncendido() {
+            return encendido;
+        }
+        public void setEncendido(boolean encendido) {
+        this.encendido = encendido;
+        }
+        public void encender(){
+        if(!encendido){
+            encendido = true;
+            System.out.println("Encendido");
+        }
+        }
+        public void apagar(){
+        if(encendido){
+            encendido = false;
+            System.out.println("apagamdp");
+        }
+        }
+            }
